718cc17e1cf9a3966b7bef9053974efb40390f52
evamaria@torch <eva-maria.odoerfer@univie.ac.at>
2024-08-09 12:27:40 +0200

    added parameter astig_angle, seperate script to test parameter series and created some data (at Sternwarte w. Diana)

---
+++ hBN_sim/.ipynb_checkpoints/helper_funcs-checkpoint.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+def timenow_formatted():
+    #get current time
+    now = datetime.now()
+    now_str = [str(now.year), str(now.month), str(now.day), str(now.hour), str(now.minute), str(now.second),]
+    
+    #make sure date has same format every time: YYYYMMDDHHMMSS
+    for i in range(len(now_str)):
+        if len(now_str[i]) == 1:
+            now_str[i] = '0' + now_str[i]
+    now_str = ''.join(now_str)
+    
+    return(now_str)
